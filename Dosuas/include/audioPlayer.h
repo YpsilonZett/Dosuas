8f4c9b77d745057549a3e92ded82e5ed2562b06d
Yorick Zeschke <yzeschke@gmail.com>
2018-03-04 18:01:45 +0100

    sweep for amplitude working; TODO: same (better) sweep principle for frequency; TODO 2: remove pointclouds (not needed any more); TODO 3: code cleanup

--- Dosuas/include/audioPlayer.h
+++ Dosuas/include/audioPlayer.h
@@ -14,12 +14,13 @@ class AudioPlayer {
 	std::vector<sf::Int16> getSineWaveSamples(float frequency, float duration, int sampeRate);
 	std::pair<std::vector<sf::Int16>, double> getSweepSamples(float fStart, float fEnd, double duration, 
 		int sampleRate, double phi0 = 0.0, int amplitude = 20000);
-	std::pair<std::vector<sf::Int16>, double> getVolumeSweepSamples(float frequency, float amp1,
-		float amp2, double duration, int sampleRate, double phi0 = 0.0);
+	std::vector<sf::Int16> getVolumeSweepSamples(float frequency, float amp1, float amp2, double duration, 
+		int sampleRate);
 	float depthToFrequency(int depth, bool useExpFunc = true);
 	int depthToAmplitude(int depth);
 	void configureSoundSource(sf::SoundBuffer& buffer, sf::Sound& sound, std::vector<sf::Int16> samples,
 		int sampleRate);
+	std::vector<sf::Int16> getChordSamples(std::vector<std::vector<int>> columns, float duration, int sampleRate);
 	std::vector<sf::Int16> getVoxelSamples(std::vector<Voxel> voxels, float duration, int sampleRate);
 public:
 	void playErrorTone(float duration, int sampleRate = 44100);
--- Dosuas/source/audioPlayer.cpp
+++ Dosuas/source/audioPlayer.cpp
@@ -7,7 +7,6 @@ std::vector<sf::Int16> AudioPlayer::getSineWaveSamples(float frequency, float du
 
 	std::vector<sf::Int16> samples;
 	const int amplitude = 20000;
-
 	for (int i = 0; i < duration * sampleRate; i++) {
 		samples.push_back(amplitude * sin(2 * PI * i * frequency / sampleRate));
 	}
@@ -19,7 +18,7 @@ std::pair<std::vector<sf::Int16>, double> AudioPlayer::getSweepSamples(float fSt
 	int sampleRate, double phi0, int amplitude) {
 	/* generates sine sweep from fStart to fEnd; allows multiple sweeps with smooth continuous phase (use phi0
 	for continuous phase, remember phi0 and set phi0 of next sweep to old phi0) */
-	
+	// TODO: better sweep ! 
 	std::vector<sf::Int16> samples;
 	const int nValues = sampleRate * duration;
 	const double actualSamplingTime = (double)nValues / (double)sampleRate;
@@ -37,37 +36,35 @@ std::pair<std::vector<sf::Int16>, double> AudioPlayer::getSweepSamples(float fSt
 }
 
 
-std::pair<std::vector<sf::Int16>, double> AudioPlayer::getVolumeSweepSamples(float frequency, float amp1,
-	float amp2, double duration, int sampleRate, double phi0) {
+std::vector<sf::Int16> AudioPlayer::getVolumeSweepSamples(float frequency, float amp1, float amp2,
+	double duration, int sampleRate) {
 	/* generates samples for sine sweep with constant frequency and changing amplitude */
 	
 	std::vector<sf::Int16> samples;
-	const double numSamples = sampleRate * duration;
-	double playedSamples, t, value;
+	const int numSamples = duration * sampleRate;
+	double t, a, y;
 	for (int i = 0; i < numSamples; i++) {
-		playedSamples = ((double)i) / numSamples;  // played samples = amplitude step 
-		t = duration * playedSamples;  
-		value = sin(2.0 * PI * t * frequency + phi0) * (amp1 + (amp2 - amp1) * playedSamples);
-		std::cout << value << ", ";
-		samples.push_back(value);
+		t = (double)i / (double)sampleRate;  // time - parameter
+		a = amp1 + (double)(amp2 - amp1) * t / duration;  // a(x) - amplitude function, linear
+		y = a * sin(t * 2.0 * PI * frequency);  // f(x) = y - wave function
+		samples.push_back(y);
 	}
-	double newPhi0 = 2.0 * PI * duration * amp2 + phi0;  // new starting phase 
-	return std::make_pair(samples, newPhi0);
+	return samples;
 }
 
 
 float AudioPlayer::depthToFrequency(int depth, bool useExpFunc) {
-	/* converts depth in cm to frequency in Hz, so that larger distance means a lower sound */
+	/* represents depth in cm as frequency in Hz, so that larger distance means a lower sound */
 	if (useExpFunc) {
 		return exp((double)-(depth - 80) * 0.0045264 + 7.74066);
-	} else {
-		return (-20.0f / 9.0f) * depth + 1300;
-	}
+	} 
+	return (-20.0f / 9.0f) * depth + 1300;
 }
 
 
 int AudioPlayer::depthToAmplitude(int depth) {
-	if (depth == 0) { 
+	/* represents depth in cm as volume (unknown amplitude unit), so that larger distances mean are more silent */
+	if ((depth < 80) || (depth > 530)) {
 		return 0; 
 	}
 	return 38533.7 * exp(-0.00819751 * depth);
@@ -127,7 +124,7 @@ void AudioPlayer::playErrorTone(float duration, int sampleRate) {
 
 
 void AudioPlayer::playSoundSwipe(std::vector<Voxel> voxels, float duration, int sampleRate) {
-	/* plays the audio swipe representing the image (using only one moving 3D sound source) */
+	/* plays the audio swipe representing the image (using only one moving 3D sound source); beginner mode */
 
 	std::vector<sf::Int16> audioSamples = getVoxelSamples(voxels, duration, sampleRate);
 	sf::Sound sound;
@@ -141,29 +138,27 @@ void AudioPlayer::playSoundSwipe(std::vector<Voxel> voxels, float duration, int 
 		xPos = -15.0f + (float)voxels[i].x / 10.0f;
 		sound.setPosition(xPos, 1, 1);
 		sf::sleep(sf::seconds(duration / voxels.size()));
-		//std::cout, << voxels[i].z << ", ";
-		//std::printf("coords: %f %f %f\n", xPos, voxels[i].y, voxels[i].z);
 	}
 }
 
 
-void AudioPlayer::playChordSwipe(std::vector<std::vector<int>> columns, float duration, int sampleRate) {
-	/* plays newer version of sound swipe, where instead of the nearest voxel, a chord of all pixel in a vertical 
-	column is played; volume corresponds to distance (louder if nearer) and frequency to voxel position on y axis */
-	std::array<float, 24> noteFrequencies = {
-		880.00, 830.61, 783.99, 739.99, 698.46, 659.25, 622.25, 587.33, 554.37, 523.25, 493.88, 466.16, 
+std::vector<sf::Int16> AudioPlayer::getChordSamples(std::vector<std::vector<int>> columns, float duration, 
+	int sampleRate) {
+	/* generates samples needed to play the chord swipe (advanced mode) by first generating sine waves with 
+	changing amplitudes of different frequencies and then adding them up to get the sequence of chords */
+
+	std::array<double, 24> noteFrequencies = {
+		880.00, 830.61, 783.99, 739.99, 698.46, 659.25, 622.25, 587.33, 554.37, 523.25, 493.88, 466.16,
 		440.00, 415.30, 392.00, 369.99, 349.23, 329.63, 311.13, 293.66, 277.81, 261.63, 246.94, 233.08
 	};
-	sf::Sound sound;
-	sf::SoundBuffer buffer;
 	std::vector<sf::Int16> samples;
 	int depthSum = 0;
 	float amp1, amp2, average1 = 0, average2 = 0;
-	double phi0 = 0.0, singleDuration = duration / ((double)columns.size() / 20.0);
-	std::pair<std::vector<sf::Int16>, double> sweep1, sweep2;
-	/*for (int i = 1; i < columns.size(); i++) {
+	double singleDuration = duration / ((double)columns.size() / 20.0);
+	std::vector<sf::Int16> connectingSweep, constantTone;
+
+	for (int i = 1; i < columns.size(); i++) {
 		depthSum += columns[i][0];
-		//std::cout << "depth: " << columns[i][0] << std::endl;
 		if (i % 10 != 0) {
 			continue;
 		}
@@ -172,26 +167,24 @@ void AudioPlayer::playChordSwipe(std::vector<std::vector<int>> columns, float du
 		amp1 = depthToAmplitude(average1);
 		amp2 = depthToAmplitude(average2);
 		average1 = average2;
-		sweep1 = getVolumeSweepSamples(noteFrequencies[0], amp1, amp2, singleDuration, sampleRate, phi0);
-		std::cout << phi0 << std::endl;
-		phi0 = sweep1.second;
-		samples.insert(samples.end(), sweep1.first.begin(), sweep1.first.end());	
-		sweep2 = getVolumeSweepSamples(noteFrequencies[0], amp2, amp2, singleDuration, sampleRate, phi0);
-		std::cout << phi0 << std::endl;
-		phi0 = sweep2.second;
-		samples.insert(samples.end(), sweep2.first.begin(), sweep2.first.end());
-	}*/
 
-	std::pair<std::vector<sf::Int16>, double> sweep, s, s2;
-	sweep = getVolumeSweepSamples(440, 1000, 20000, duration, sampleRate);
-	s = getVolumeSweepSamples(440, 20000, 20000, duration, sampleRate, sweep.second);
-	s2 = getVolumeSweepSamples(440, 20000, 7000, duration, sampleRate, s.second);
-	samples = sweep.first;
-	samples.insert(samples.end(), s.first.begin(), s.first.end());
-	samples.insert(samples.end(), s2.first.begin(), s2.first.end());
+		connectingSweep = getVolumeSweepSamples(noteFrequencies[0], amp1, amp2, singleDuration, sampleRate);
+		samples.insert(samples.end(), connectingSweep.begin(), connectingSweep.end());
 
+		constantTone = getVolumeSweepSamples(noteFrequencies[0], amp2, amp2, singleDuration, sampleRate);
+		samples.insert(samples.end(), constantTone.begin(), constantTone.end());
+	}
+	return samples;
+}
+
+
+void AudioPlayer::playChordSwipe(std::vector<std::vector<int>> columns, float duration, int sampleRate) {
+	/* plays newer swipe version (advanced mode) using chords, volume and 3d position */
+	
+	sf::Sound sound;
+	sf::SoundBuffer buffer;
+	std::vector<sf::Int16> samples = getChordSamples(columns, duration, sampleRate);
 	configureSoundSource(buffer, sound, samples, sampleRate);
 	sound.play();
 	sf::sleep(sf::seconds(duration));
-	sf::sleep(sf::seconds(0.5));
 }
